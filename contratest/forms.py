64cb10954d887f67e8fc4ede815efc7b194320f9
maianess <maia.mcc@gmail.com>
2014-08-19 12:34:55 -0400

    sbare-bones search form complete

--- contratest/forms.py
+++ contratest/forms.py
@@ -14,9 +14,8 @@ class MoveForm(forms.Form):
     hand = EmptyChoiceField(required=False, widget=forms.Select, choices=Move.HAND_CHOICES)
     dir = EmptyChoiceField(required=False, widget=forms.Select, choices=Move.DIR_CHOICES)
     dist = forms.DecimalField(max_digits=3, decimal_places=2, initial=None)
-    bal = forms.NullBooleanField(initial=None)
+    bal = EmptyChoiceField(required=False, widget=forms.Select, choices=[(1, True), (0, False)])
 
 # to-do:
-# make 'bal' actually be nullable
 # logic about which forms/choices appear where
 # make addl choices for sect: A1, A2, B1, B2, A, B
