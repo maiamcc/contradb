ac476f864415ace5d1b9330877f8811403c4a805
maianess <maia.mcc@gmail.com>
2014-08-20 18:09:46 -0400

    dance parser fiddling

--- contratest/dance_parser.py
+++ contratest/dance_parser.py
@@ -1,4 +1,4 @@
-# from contratest.models import *
+from contratest.models import Dance, Move
 import re
 
 # break into lines
@@ -25,7 +25,8 @@ def make_dance(dance_string):
 def clean_moves_list(moves_list):
     newlist = []
     for element in moves_list:
-        newstring = re.search('(?<=[AB][12]: ).*', element).group()
+
+        newstring = re.search('(?<=[ab][12]: ).*', element.lower()).group()
         newlist.append(newstring.split("; "))
 
     return newlist
@@ -36,13 +37,51 @@ def make_move(move_string, dance):
 
 
 def get_movename(move_string):
-    pass
+    for key in movename_dict.keys():
+        if move_string.find(key) > -1:
+            return movename_dict[key]
+    else:
+        return raw_input("I can't tell; what move is this?\n>")
 
-text = file_to_string("babyrose.txt")
+
+text = file_to_string("contratest/babyrose.txt")
 dance_list = break_input(text)
 dance = make_dance(dance_list[0])
-clean_moves = clean_moves_list(dance_list[1:])
+#clean_moves = clean_moves_list(dance_list[1:])
 
+movename_dict = {
+    "swing": "swing",
+    "b&s": "swing",
+    "gypsy": "gypsy",
+    "dsd": "dosido",
+    "dosido": "dosido",
+    "do-si-do": "dosido",
+    "alle": "allemande",
+    "turn by": "allemande", #this is the hardest!
+    "star": "star",
+    "circle": "circle",
+    "long lines": "longlines",
+    "f&b": "longlines",
+    "ll ": "longlines",
+    "chain": "chain",
+    "down the hall": "down_hall", #turning must be own move
+    "prom": "promenade"
+}
+
+who_dict = {
+    "ladies": "ladies",
+    "woman": "ladies",
+    "gents": "gents",
+    "men": "men",
+    "n ": "neighbor",
+    "ns": "neighbor",
+    "neighbor": "neighbor",
+    "P ": "partner",
+    "ps": "partner",
+    "partner": "partner",
+    "shad": "shadow",
+    "shadow": "shadow"
+}
 '''
 # list of what values each move expects
 expected_values = {
@@ -67,4 +106,4 @@ expected_values = {
     "other": []}
     '''
 
-
+# http://www.cotellese.net/2007/09/27/running-external-scripts-against-django-models/
