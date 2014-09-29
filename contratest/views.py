0982a25783c1f546121420ad140160256217531c
Maia <maia.mcc@gmail.com>
2014-09-29 16:37:11 -0400

    get request converted directly to nested list; pretty print method for nested list

--- contratest/views.py
+++ contratest/views.py
@@ -20,56 +20,53 @@ def search(request):
     return render(request, 'contratest/search.html', context)
 
 def results(request):
-    def pretty_repr(d):
-        """For debug."""
-        for k, v in d.iteritems():
-            print k
-            for k1, v1 in v.iteritems():
-                print "\t",k1
-                for k2, v2 in v1.iteritems():
-                    print "\t\t",k2,"-->",v2
-
-    def pretty_print(d):
-        results = []
-        for i, v in enumerate(d.values()):
-            if i != 0:
-                results.append("and")
-            for k1, v1 in v.iteritems():
-                results.append("<pre>   %s=%s</pre>" % ("movename", v1["movename"]))
-                for k2, v2 in v1.iteritems():
-                    if k2 != "movename":
-                        if v2:
-                            val = v2
-                        else:
-                            val = "[any]"
-                        results.append("<pre>        %s=%s</pre>" % (k2, val))
-        return results
-
-    def get_reqest_to_nested_dict(req):
-        """Turns given GET request into a nested dict."""
-        results = defaultdict(lambda : defaultdict(dict))
-        for k, v in req.GET.iteritems():
-            if not (k.startswith("csrf")):
-                results[k[-1]][int(k[-2])][k[:-2]] = v
-        return results
-
-    def nested_dict_to_list(dict):
-        """Turns a nested dict of search terms into a list of lists of dicts.
-        e.g. [[A,B],[C],[D,E]] where A = {"move" : "foo", "who" : "bar"} etc.
-        and [A,B] = <move defined by move query A> followed by
-        <move defined by move query B>"""
-        results = []
-        for k, v in sorted(dict.iteritems()):
-            subresults = []
-            for k1, v1 in sorted(v.iteritems()):
-                subresults.append(v1)
-            results.append(subresults)
-        return results
 
     # TERMINOLOGY:
-    # {"move" : "swing", "who" : "partner"} = move_query (a dict. representing a collection of attributes that points to a specific move)
-    # [{"move" : "foo", "who" : "bar"}, {"move" : "baz", "hand" : "R"}] = query_sequence
-    # [[{"move" : "foo", "who" : "bar"}, {"move" : "baz", "hand" : "R"}], [{"move" : "swing", "who" : "partner"}]] = query_set
+        # {"move" : "swing", "who" : "partner"} = move query -- a dict. representing a
+            # collection of attributes that points to a specific move
+        # [{"move" : "foo", "who" : "bar"}, {"move" : "baz", "hand" : "R"}] = query sequence --
+            # one or more move queries to be searched for in sequence (e.g. move A followed
+            # by move B)
+        # [[{"move" : "foo", "who" : "bar"}, {"move" : "baz", "hand" : "R"}],
+            # [{"move" : "swing", "who" : "partner"}]] = query set -- multiple query sequences
+            # and'ed together (e.g. <move A followed by move B> AND <move C>)
+
+    def make_nested_list(req):
+        sorted_search_terms = sorted(req.GET.items(), key=lambda kv: kv[0][-2])[:-1]
+        last_num = None
+        last_let = None
+        results = []
+        for attr, val in sorted_search_terms:
+            cur_num = attr[-2]
+            cur_let = attr[-1]
+            if cur_let != last_let:
+                results.append([])
+
+            if cur_num != last_num:
+                results[-1].append({attr[:-2] : val})
+            else:
+                results[-1][-1][attr[:-2]] = val
+
+            last_num = cur_num
+            last_let = cur_let
+        return results
+
+    def pretty_print(search_list):
+        results = []
+        for i, query_seq in enumerate(search_list):
+            if i != 0:
+                results.append("and")
+
+            for move_query in query_seq:
+                results.append("<pre>   %s=%s</pre>" % ("movename", move_query["movename"]))
+                for attr, val in sorted(move_query.iteritems()):
+                    if attr != "movename":
+                        if val:
+                            val_text = val
+                        else:
+                            val_text = "[any]"
+                        results.append("<pre>        %s=%s</pre>" % (attr, val_text))
+        return results
 
     def find_next_moves(moves_list):
         """Given a moves list, returns a list of moves that come directly after given moves
@@ -136,16 +133,11 @@ def results(request):
             result_dances.append(move.dance)
         return list(set(result_dances))
 
-    # make a nested dict
-    searched_for = get_reqest_to_nested_dict(request)
+    # make nested list from GET request, format prettily for screen
+    search_term_list = make_nested_list(request)
+    pretty_search_terms = pretty_print(search_term_list)
 
-    # send pretty formatting to page, print to console
-    pretty_search_terms = pretty_print(searched_for)
-    pretty_repr(searched_for)
-
-    # make nested dict into a list
-    search_term_list = nested_dict_to_list(searched_for)
-
+    # find dances that meet search criteria
     moves_found = resolve_query_set(search_term_list)
     dances = find_dances(moves_found)
 
